6aec4a1c4e56d4a8817b1ffa36db2e8b7f2a6c78
Ian Draves <ian.draves@cyberline.com>
2022-10-28 14:46:44 -0600

    Adding base files

---
+++ index.js
@@ -0,0 +1,119 @@
+const express = require("express");
+const app = express();
+const pgp = require("pg-promise")();
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const axios = require("axios");
+
+// Database configuration
+const dbConfig = {
+    host: "db",
+    port: 5432,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+};
+const db = pgp(dbConfig);
+
+// Testing database
+db.connect()
+    .then((obj) => {
+        console.log("Database connection successful"); // you can view this message in the docker compose logs
+        obj.done(); // success, release the connection;
+    })
+    .catch((error) => {
+        console.log("ERROR:", error.message || error);
+    });
+
+// EJS configuration
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        saveUninitialized: false,
+        resave: false,
+    })
+);
+
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+
+// GET requests
+app.get("/", (req, res) => {
+    res.redirect("/login");
+});
+
+app.get("/login", (req, res) => {
+    res.render("pages/login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("pages/register");
+});
+
+app.get("/account", async (req, res) => {
+    res.redirect('pages/account');
+});
+
+app.get("/logout", (req, res) => {
+    req.session.destroy();
+    res.render("pages/login");
+});
+
+// POST requests
+app.post("/register", async (req, res) => {
+    const hash = await bcrypt.hash(req.body.password, 10);
+
+    const query = "INSERT INTO users (username, password) VALUES ($1, $2);";
+    db.any(query, [req.body.username, hash])
+        .then(function (data) {
+            res.redirect("/login");
+        })
+        .catch(function (err) {
+            res.redirect("/register");
+            return console.log(err);
+        });
+});
+
+app.post("/login", (req, res) => {
+    const query = "SELECT password FROM users WHERE username = $1;";
+    db.one(query, [req.body.username])
+        .then(async function (user) {
+            const match = await bcrypt.compare(req.body.password, user.password);
+
+            if (match) {
+                req.session.user = {
+                    api_key: process.env.API_KEY,
+                };
+                req.session.save();
+
+                res.redirect("/account");
+            } else {
+                throw Error("Incorrect username or password.");
+            }
+        })
+        .catch(function (err) {
+            res.redirect("/login");
+            return console.log(err);
+        });
+});
+
+// Authentication middleware
+const auth = (req, res, next) => {
+    if (!req.session.user) {
+        return res.redirect('/register');
+    }
+    next();
+};
+
+// Authentication required
+app.use(auth);
+
+// Listen on port 3000
+app.listen(3000);
+console.log("Server is listening on port 3000");
