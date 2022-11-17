5eb267ecb42c398085ebe47c1e9a18f943d52120
Sanjana Kumpati <saku3312@colorado.edu>
2022-11-17 00:11:10 -0700

    Deleting the transaction table.

--- project_code/init_data/create.sql
+++ project_code/init_data/create.sql
@@ -7,10 +7,3 @@ CREATE TABLE users(
     last_name VARCHAR(50),
     email VARCHAR(50)
 );
-
-CREATE TABLE transactions(
-    transaction_id INT PRIMARY KEY,
-    requester VARCHAR(50),
-    sender VARCHAR(50),
-    meals_count INT
-);
