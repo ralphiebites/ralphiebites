11d07f2a2dac09ba2afab1c41421a3019cfd192f
Sanjana Kumpati <saku3312@colorado.edu>
2022-11-17 00:26:42 -0700

    Deleting transacation table.

--- project_code/init_data/create.sql
+++ project_code/init_data/create.sql
@@ -1,9 +1,10 @@
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users(
     student_id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
+    username VARCHAR(50),
     password CHAR(60) NOT NULL,
-    first_name VARCHAR(50) NOT NULL,
+    first_name VARCHAR(50),
     last_name VARCHAR(50),
     email VARCHAR(50)
 );
+
