b6227116bee22f6407a8725d075373b1e0dd104b
Ian Draves <ian.draves@cyberline.com>
2022-11-18 14:31:24 -0700

    Merged market_page-05 branch on local to prepare for pull request

--- project_code/init_data/create.sql
+++ project_code/init_data/create.sql
@@ -1,7 +1,6 @@
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users(
-    student_id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
+    username VARCHAR(8) NOT NULL,
     password CHAR(60) NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50),
